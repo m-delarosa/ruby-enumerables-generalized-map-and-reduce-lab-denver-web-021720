e08ef3d8144c881a94b2230b9e18a0aababf1f01
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-13 03:49:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,5 +13,12 @@ def map(source_array)
 end
 
 def reduce (array, starting_point = nil)
+  if starting_point
+    num1 = starting_point
+    i = 0
+  else
+    num1 = array[0]
+    i = 1
+  end
   
 end
