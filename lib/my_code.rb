20baf8436dd9c37f0be45df5c96f2009134362df
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-13 04:01:56 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+
+def map(source_array)
+  new = []
+  i = 0
+
+  while i < source_array.length do
+    new << yield(source_array[i])
+    i += 1
+  end
+
+  new
+end
+
+def reduce (array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+
+  sum
+end
